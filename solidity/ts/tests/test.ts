6d8aefe0eb3a846acaa10475df1817fb0959219c
nuevoalex <achap5dk@gmail.com>
2025-07-11 10:06:59 -0700

    commit test notes to direct dev

--- solidity/ts/tests/test.ts
+++ solidity/ts/tests/test.ts
@@ -20,4 +20,29 @@ describe('Contract Test Suite', () => {
 		const isDeployed = await isSisypheanExchangeDeployed(client)
 		assert.ok(isDeployed, `Not Deployed!`)
 	})
+
+	// Can create a market
+
+	// Can buy complete sets
+
+	// Can cash out complete sets
+
+	// Market can resolve and distribute Cash
+
+	// Market can be disputed and resolve
+
+	// Market can be disputed and fork
+		// Cash token forks
+		// All markets fork
+		// Forking market is finalized
+		// Other markets return to pre-reporting state
+		// REP migration
+		// Cash migration
+			// Cash migrated proportionally to REP migration
+			// Remaining Cash distributed to REP holders
+		// Cash for REP Auction
+		    // (A) All succeed
+			// (B) One fails
+			// (C) Two fail
+			// (D) All fail
 })
